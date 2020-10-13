2aad0ed4ccd5987dd1a5246531b532456741862d
Ashley Moore <mooream2291@gmail.com>
2020-10-12 20:31:40 -0400

    x

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3000;
 app.use(CORS());
 
 app.get('/location', (request, response) => {
-  const getLocation = require('data/location.json');
+  const getLocation = require('./data/location.json');
   const searchQuery = request.params.city;
   const newLocation = new Location(getLocation[0], searchQuery);
   response.send(newLocation);
@@ -18,15 +18,15 @@ app.get('/location', (request, response) => {
 });
 
 function Location (city, search) {
-  this.search_query = search
+  this.search_query = search;
   this.formatted = city.display_name;
-  this.latitude = city.lat
-  this.longitude = city.lon
+  this.latitude = city.lat;
+  this.longitude = city.lon;
 }
 
 app.listen(PORT, () => {
-    console.log(`{PORT}`);
-  });
+  console.log(`{PORT}`);
+});
 
 
 
